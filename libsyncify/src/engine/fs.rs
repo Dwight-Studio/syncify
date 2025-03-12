95f8a1c25ebf4bf477889b904a4a1d6f8b3138cf
Alexandre Leconte <aleconte@dwightstudio.fr>
2025-03-13 00:11:53 +0100

    Fixed EventProcessor blocking at drop

--- libsyncify/src/engine/fs.rs
+++ libsyncify/src/engine/fs.rs
@@ -4,6 +4,7 @@ use notify::EventKind::{Create, Modify, Remove};
 use notify::{Event, EventHandler};
 use std::ops::Deref;
 use std::sync::Arc;
+use tokio::io::AsyncWriteExt;
 use tokio::sync::{mpsc, RwLock};
 use tokio::task::JoinHandle;
 
@@ -11,16 +12,14 @@ const NOTIFICATION_BUFFER_SIZE: usize = 1024;
 
 /// Actor responsible to handle all filesystem events.
 pub struct EventProcessor {
-    task_handle: Option<JoinHandle<()>>,
     handle: EventProcessorHandle,
 }
 
 impl EventProcessor {
     pub fn new(config: Arc<RwLock<StoreManager>>) -> Self {
         let (tx, rx) = mpsc::channel(NOTIFICATION_BUFFER_SIZE);
-
+        tokio::spawn(Self::handle_event(config, rx));
         Self {
-            task_handle: Some(tokio::spawn(Self::handle_event(config, rx))),
             handle: EventProcessorHandle { tx },
         }
     }
@@ -63,16 +62,6 @@ impl Deref for EventProcessor {
 
     fn deref(&self) -> &Self::Target {
         &self.handle
-    }
-}
-
-impl Drop for EventProcessor {
-    fn drop(&mut self) {
-        if let Err(error) =
-            futures::executor::block_on(async { self.task_handle.take().unwrap().await })
-        {
-            log::error!("Failed to join thread: {error}");
-        }
     }
 }
 
