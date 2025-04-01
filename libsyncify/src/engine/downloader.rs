7abbb2b005af64216955c36cd9888a7014a2969d
gamermine <gamermine@dwightstudio.fr>
2025-04-01 20:31:18 +0200

    Downloader
    - Oui

--- libsyncify/src/engine/downloader.rs
+++ libsyncify/src/engine/downloader.rs
@@ -158,10 +158,16 @@ impl Downloader {
                             }
                         };
 
-                        if let Ok(mut file) = File::open(file_path.clone()) {
-                            if let Err(err) = std::io::copy(&mut file, &mut encoder) {
-                                error!("Cannot encode: {err}");
-                                return;
+                        if let Ok(file) = File::open(file_path.clone()) {
+                            let mut reader = BufReader::new(file);
+                            let mut buf = [0u8; CHUNK_SIZE];
+                            while let Ok(len) = reader.read(&mut buf) {
+                                if len == 0 {
+                                    break;
+                                }
+                                if let Err(err) = encoder.write(&buf[0..len]) {
+                                    error!("Cannot write to cache file: {err}");
+                                }
                             }
 
                             match encoder.finalize() {
@@ -180,7 +186,7 @@ impl Downloader {
                                 Err(err) => {
                                     error!("Error while finalizing file: {err}");
                                 }
-                            } 
+                            }
                         }
                     } else {
                         warn!("Received local provision update for unknown UUID: {}", dir_uuid);
